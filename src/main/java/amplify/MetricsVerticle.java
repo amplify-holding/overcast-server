4ab8ffe5561e3c01a252cf882b24c14b08faa8d3
Flynn Jones <fjones+accessdevteam@amplify.com>
2014-09-08 19:59:42 -0400

    Initial setup of the Vert.x Overcast Server with Maven integration and running tests. Two verticles have been created. Once for internal pinging and another for http endpoints.

---
+++ src/main/java/amplify/MetricsVerticle.java
@@ -0,0 +1,21 @@
+package amplify;
+
+import org.vertx.java.core.Handler;
+import org.vertx.java.core.eventbus.Message;
+import org.vertx.java.core.http.HttpServerRequest;
+import org.vertx.java.platform.Verticle;
+
+public class MetricsVerticle extends Verticle {
+    public void start() {
+        vertx.createHttpServer().requestHandler(new Handler<HttpServerRequest>() {
+            public void handle(HttpServerRequest request) {
+                container.logger().info("A request has arrived on the server!");
+                request.response().end();
+                vertx.eventBus().send("ping-address", "ping!");
+            }
+        }).listen(8200, "localhost");
+
+        container.logger().info("MetricsVerticle started");
+        container.deployVerticle("PingVerticle.java");
+    }
+}
